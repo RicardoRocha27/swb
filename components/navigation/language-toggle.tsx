62f690fd26c63e881ca4c77153869856c0fb59f0
Tiago Pereira <tiagorp2004@gmail.com>
2023-11-01 13:12:31 +0000

    lang toggle advances, not done yet

--- components/navigation/language-toggle.tsx
+++ components/navigation/language-toggle.tsx
@@ -2,19 +2,36 @@
 import React, { useState } from "react";
 import { Switch } from "@/components/ui/switch";
 import { cn } from "@/lib/utils";
+import { usePathname, useRouter } from "next/navigation";
+import { CustomLocale } from "@/types";
 
 const LanguageToggle = () => {
   const [language, setlanguage] = useState("pt");
   const changeLanguage = () => {
     language === "en" ? setlanguage("pt") : setlanguage("en");
   };
+  const router = useRouter();
+  const pathname = usePathname();
+  const currentLocale = pathname.split("/")[1] as CustomLocale;
+
+  const handleChangeLocale = (locale: CustomLocale) => {
+    const path = pathname.replace(currentLocale, locale);
+    router.push(path);
+  };
 
   return (
     <div className={cn("flex  space-x-2 w-fit")}>
-      <Switch onClick={changeLanguage} className="mx-auto" />
+      <Switch
+        onClick={() =>
+          currentLocale === "pt"
+            ? handleChangeLocale("en")
+            : handleChangeLocale("pt")
+        }
+        className="mx-auto"
+      />
 
       <p className={cn("mx-auto font-medium text-base")}>
-        {language === "pt" ? "Português" : "English"}
+        {currentLocale === "pt" ? "Português" : "English"}
       </p>
     </div>
   );
