cf6a51da491f3758c7c43a7e14249ec63a2399b3
RicardoRocha27 <rocharicardo2799@gmail.com>
2023-11-03 13:04:20 +0000

    faqs in dictionary

--- components/faqs/faqs.tsx
+++ components/faqs/faqs.tsx
@@ -17,35 +17,16 @@ const Faqs = async ({ locale }: { locale: CustomLocale }) => {
       <Container>
         <h2 className="text-xl font-bold pb-24">{faqs.title}</h2>
         <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="text-start">
-              How can I check on the progress of my project?
-            </AccordionTrigger>
-            <AccordionContent className="text-start">
-              Don&apos;t worry, we share our files with you so you can check it
-              whenever you desire!
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2">
-            <AccordionTrigger className="text-start">
-              Is there any way for the client to be able to change the website
-              after the delivery?
-            </AccordionTrigger>
-            <AccordionContent className="text-start">
-              Of course, no website is completely static! Normally for the post
-              delivery part we agree with the client the terms if there is the
-              need to change anything later down the line.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-3">
-            <AccordionTrigger className="text-start">
-              What is the delivery time for a project?
-            </AccordionTrigger>
-            <AccordionContent className="text-start">
-              It broadly depends on the project and if it is a design, a
-              development project or maybe both.
-            </AccordionContent>
-          </AccordionItem>
+          {Object.entries(faqs.questions).map((item, index) => (
+            <AccordionItem key={index} value={`item-${index + 1}`}>
+              <AccordionTrigger className="text-start">
+                {item[1].question}
+              </AccordionTrigger>
+              <AccordionContent className="text-start">
+                {item[1].answer}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
         </Accordion>
       </Container>
     </div>
