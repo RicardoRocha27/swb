8e1ec02bd94779f1fc73c00ee1d47677ff7fa35b
RicardoRocha27 <rocharicardo2799@gmail.com>
2023-11-12 16:52:26 +0000

    navigation hover effects added

--- components/navigation/menu-item.tsx
+++ components/navigation/menu-item.tsx
@@ -19,11 +19,14 @@ const MenuItem = ({ item }: TMenuItem) => {
   };
 
   return (
-    <div
-      onClick={() => handleScroll(item.path)}
-      className="text-sm md:text-base cursor-pointer rounded-md ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-    >
-      {item.label}
+    <div className="relative group">
+      <div
+        onClick={() => handleScroll(item.path)}
+        className="text-sm md:text-base cursor-pointer group-hover:text-primary duration-300 ring-offset-background transition-colors focus:rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+      >
+        {item.label}
+      </div>
+      <div className="absolute w-0 md:group-hover:w-full group-hover:origin-middle bottom-0 left-1/2 transform -translate-x-1/2 h-[2px] bg-primary transition-all duration-300" />
     </div>
   );
 };
